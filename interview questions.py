a0c3e178c7572a8d3c8328cb453626e96cfb2c39
SUDHARANIJANGA <122001053+SUDHARANIJANGA@users.noreply.github.com>
2023-03-04 21:50:12 +0530

    Add files via upload

---
+++ interview questions.py
@@ -0,0 +1,20 @@
+Differentiate between mutable and immutable objects?
+What is Variable in Python?
+How can we assign the same value to multiple variables in one single go?
+What are the methods available for the conversion of numbers from one type to another?
+What are the mathematical functions defined in Python to work with numbers?
+What are Arithmetic Operators? What are various types of arithmetic operators that we can use in Python?
+What is the Arithmetic operator’s precedence in Python?
+What are relational operators?
+What are assignment operators?
+What are logical operators
+what is pass in python?
+what does the continues do in python?
+when should you use The Break in python?
+what gets printed?
+what is output?
+which numbers are printed?
+what is a built in function that python uses to literarte ovwe a number sequence?
+what is lambda in python?
+mention the use of the split function in python?
+what is a function in python programming?
